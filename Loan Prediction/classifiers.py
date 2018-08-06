8b4961491c6bb54cd0eb76d9cc43f95b418e7d22
hari304 <37880992+hari304@users.noreply.github.com>
2018-08-06 12:54:44 +0530

    Add files via upload

--- Loan Prediction/classifiers.py
+++ Loan Prediction/classifiers.py
@@ -58,8 +58,10 @@ class logisticRegression(object):
             false_nagative = np.count_nonzero(pred[(pred == 0) & (pred !=y)]==0)
             sensitivity[i] = true_positive/(true_positive+false_nagative)
             one_minus_specificity[i] = 1 - (true_negative/(true_negative+false_positive))
+        sensitivity = np.sort(sensitivity)
+        one_minus_specificity = np.sort(one_minus_specificity)
         if show_cm:
-            plt.scatter(one_minus_specificity,sensitivity)
+            plt.plot(one_minus_specificity,sensitivity)
             plt.xlabel('1-specificity')
             plt.ylabel('sensitivity')
             plt.title('ROC')
@@ -148,13 +150,13 @@ class DecisionTree(object):
             entropy -= (c)*np.log2(c)
         return entropy
     
-    def get_split_node(self):
-        data_set_entropy = self.entropy(self.train_arr[:,-1])
+    def get_split_node(self,train_arr):
+        data_set_entropy = self.entropy(train_arr[:,-1])
         #print('data_set_entropy',data_set_entropy)
         num_record = self.train_arr.shape[0]
-        feature_info_gain = np.zeros((self.train_arr.shape[1]-1))
+        feature_info_gain = np.zeros((train_arr.shape[1]-1))
         for i in range(self.train_arr.shape[1]-1):
-            train_feat_arr = self.train_arr[:,[i,-1]]
+            train_feat_arr = train_arr[:,[i,-1]]
             feature_entropy = 0.0
             categories = np.unique(train_feat_arr[:,0])
             for cat in categories:
@@ -165,17 +167,76 @@ class DecisionTree(object):
         #print('feature_info_gain',feature_info_gain)
         return np.argmax(feature_info_gain)
     
-    def predict(self,x):
-        train_filter = self.train_arr
-        while True:
-             train_filter = train_filter[:,(self.Dtree[0,0] = x[self.Dtree[0,0]])]
-             
-            break
-            
+    def predict(self,test_x):
+        pred = []
+        for x in test_x:
+            #print(x)
+            decision_node = self.Dtree[0,0]
+            filter_train = self.train_arr
+            #print('self.Dtree',self.Dtree)
+            #print('filter_train.shape',filter_train.shape)
+            while decision_node is not -1:
+                #print('self.Dtree[:,0]',self.Dtree[:,0])
+                #print('decision_node',decision_node)
+                split_cat = x[decision_node]
+                #print('self.Dtree[:,1]',self.Dtree[:,1])
+                #print('split_cat',split_cat)
+                #print('np.array_str(self.Dtree[:,1]) == str(split_cat)',self.Dtree[:,1].astype(str) == str(split_cat))
+                filter_train = filter_train[filter_train[:,decision_node].astype(str) == str(split_cat)]
+                #print('filter_train.shape',filter_train.shape)
+                #print(self.Dtree[(self.Dtree[:,0] == decision_node) & (self.Dtree[:,1].astype(str) == str(split_cat))])
+                decision_node = np.asscalar(self.Dtree[(self.Dtree[:,0] == decision_node) 
+                                                       & (self.Dtree[:,1].astype(str) == str(split_cat))][:,-1])
+            yesprop = np.count_nonzero(filter_train[:,-1] =='Y')/filter_train.shape[0]
+            pred.append(yesprop)
+        pred = np.array(pred)
+        return pred
+        
+    def score(self,X,y,show_cm=False):
+        pred_prob = self.predict(X)
+        rand_num = 10000
+        epsilon = 1e-20
+        np.place(y,y=='Y',[1])
+        np.place(y,y=='N',[0])
+        cut_offs = np.random.rand(rand_num)
+        #cut_offs = [0.5]
+        sensitivity = np.zeros((rand_num))
+        one_minus_specificity =np.zeros((rand_num))
+        for i in range(len(cut_offs)):
+            #print('i',i)
+            #print('cut_offs[i]',cut_offs[i])
+            #print('pred_prob',pred_prob)
+            pred =np.array([pred_prob>=cut_offs[i]],dtype=np.int32)
+            pred = pred.flatten()
+            #print('pred',pred)
+            true_positive = np.count_nonzero(pred[(pred==1) & (pred == y)])
+            #print('true_positive',true_positive)
+            false_positive = np.count_nonzero(pred[(pred==1) & (pred != y)])
+            #print('false_positive',false_positive)
+            true_negative = np.count_nonzero(pred[(pred==0) & (pred == y)]==0)
+            #print('true_negative',true_negative)
+            false_nagative = np.count_nonzero(pred[(pred == 0) & (pred !=y)]==0)
+            #print('false_nagative',false_nagative)
+            sensitivity[i] = true_positive/(true_positive+false_nagative+epsilon)
+            #print('sensitivity[i]',sensitivity[i])
+            one_minus_specificity[i] = 1 - (true_negative/(true_negative+false_positive+epsilon))
+            #print('one_minus_specificity[i]',one_minus_specificity[i])
+        sensitivity = np.sort(sensitivity)
+        one_minus_specificity = np.sort(one_minus_specificity)
+        if show_cm:
+            plt.plot(one_minus_specificity,sensitivity)
+            plt.xlabel('1-specificity')
+            plt.ylabel('sensitivity')
+            plt.title('ROC')
+            plt.show()
+        auc = metrics.auc(one_minus_specificity,sensitivity,reorder=True)
+        #print('Area Under the Curve',auc)
+        return auc
+    
         
     def fit(self,train_ds,max_node_points=2,verbose=False):
         self.train_arr = train_ds.values
-        split_node = self.get_split_node()
+        split_node = self.get_split_node(self.train_arr)
         split_node_cat = np.unique(self.train_arr[:,split_node])
         self.Dtree = np.transpose(np.vstack(([split_node]*len(split_node_cat),split_node_cat,[-1]*len(split_node_cat))))
         #print('Dtree',Dtree)
